5c2b91e00e31776fcf5fdbda3b2af682b3187a45
Dausoi <atokedokee@gmail.com>
2023-10-04 16:50:46 +0000

    delete unused file

--- download_files.py
+++
@@ -1,9 +0,0 @@
-"""
-1. Download xml file locally
-2. Download pages from the xml file, only relevant to the html scraping
-3. Upload to the data lake (i.e. Google Cloud Storage)
-"""
-
-from pathlib import Path
-from datetime import datetime
-import requests
